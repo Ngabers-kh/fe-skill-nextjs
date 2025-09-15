340b522f5d10b152be51ab05ecb9565877c0a92d
Xinqi04 <155415026+Xinqi04@users.noreply.github.com>
2025-09-15 20:46:12 +0700

    first commit

---
+++ app/components/navbar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav>
+      {/* Isi navbar di sini */}
+      <h1 className="bg-zinc-600">Navbar</h1>
+    </nav>
+  );
+};
+
+export default Navbar;
