c2d3c7b1d2cee16f0c77bbcabbd014117fcc0ced
fauzanashshidiq <muhzeen07@gmail.com>
2025-09-23 22:10:23 +0700

    feat(skill page): responsive

--- app/skill/page.tsx
+++ app/skill/page.tsx
@@ -45,34 +45,40 @@ export default function SkillPage() {
 
   return (
     <div
-      className="min-h-screen flex flex-col items-center justify-center bg-cover bg-center"
+      className="w-full min-h-screen items-center justify-center flex flex-col bg-cover bg-center"
       style={{ backgroundImage: "url('/bgRegister.png')" }}
     >
-      <h1 className="text-3xl font-bold mb-10">Fill in Your Skill</h1>
+      <div className="max-w-7xl mx-auto p-9 sm:p-8 md:p-6">
+        <h1 className="text-3xl font-bold mb-10 text-center">
+          Fill in Your Skill
+        </h1>
 
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 max-w-4xl">
-        {skills.map((skill, idx) => (
-          <button
-            key={idx}
-            onClick={() => toggleSkill(skill)}
-            className={`px-4 py-2 rounded-full font-medium shadow-md transition 
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 max-w-4xl">
+          {skills.map((skill, idx) => (
+            <button
+              key={idx}
+              onClick={() => toggleSkill(skill)}
+              className={`px-4 py-2 rounded-full font-medium shadow-md transition 
               ${
                 selectedSkills.includes(skill)
                   ? "bg-blue-600 text-white"
                   : "bg-[#FFEEAA] text-black hover:bg-yellow-400"
               }`}
-          >
-            {skill}
-          </button>
-        ))}
-      </div>
+            >
+              {skill}
+            </button>
+          ))}
+        </div>
 
-      <button
-        onClick={handleNext}
-        className="mt-10 px-6 py-2 rounded-lg bg-green-500 hover:bg-green-600 text-white font-medium shadow"
-      >
-        Next →
-      </button>
+        <div className="flex justify-center mt-10">
+          <button
+            onClick={handleNext}
+            className="px-6 py-2 rounded-lg bg-blue-600 hover:bg-blue-500 text-white font-medium shadow"
+          >
+            Next →
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
